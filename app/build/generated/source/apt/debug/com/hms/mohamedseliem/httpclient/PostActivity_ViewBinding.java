4b4fd4f3125205f5248aaea7de29c3bff9d0c70b
Zobaed Sm <WIN\c00300901@moshenlab3.win.louisiana.edu>
2017-11-12 22:27:41 -0600

    splash screen UI + test

---
+++ app/build/generated/source/apt/debug/com/hms/mohamedseliem/httpclient/PostActivity_ViewBinding.java
@@ -0,0 +1,52 @@
+// Generated code from Butter Knife. Do not modify!
+package com.hms.mohamedseliem.httpclient;
+
+import android.support.annotation.CallSuper;
+import android.support.annotation.UiThread;
+import android.view.View;
+import android.widget.TextView;
+import butterknife.Unbinder;
+import butterknife.internal.DebouncingOnClickListener;
+import butterknife.internal.Utils;
+import java.lang.IllegalStateException;
+import java.lang.Override;
+
+public class PostActivity_ViewBinding implements Unbinder {
+  private PostActivity target;
+
+  private View view2131296318;
+
+  @UiThread
+  public PostActivity_ViewBinding(PostActivity target) {
+    this(target, target.getWindow().getDecorView());
+  }
+
+  @UiThread
+  public PostActivity_ViewBinding(final PostActivity target, View source) {
+    this.target = target;
+
+    View view;
+    target.title = Utils.findRequiredViewAsType(source, R.id.post_title, "field 'title'", TextView.class);
+    view = Utils.findRequiredView(source, R.id.btn_posts, "method 'getPosts'");
+    view2131296318 = view;
+    view.setOnClickListener(new DebouncingOnClickListener() {
+      @Override
+      public void doClick(View p0) {
+        target.getPosts();
+      }
+    });
+  }
+
+  @Override
+  @CallSuper
+  public void unbind() {
+    PostActivity target = this.target;
+    if (target == null) throw new IllegalStateException("Bindings already cleared.");
+    this.target = null;
+
+    target.title = null;
+
+    view2131296318.setOnClickListener(null);
+    view2131296318 = null;
+  }
+}
---
+++ app/src/main/java/com/hms/mohamedseliem/httpclient/SplashActivity.java
@@ -0,0 +1,54 @@
+package com.hms.mohamedseliem.httpclient;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.ProgressBar;
+
+import com.felipecsl.gifimageview.library.GifImageView;
+
+import org.apache.commons.io.IOUtils;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+public class SplashActivity extends AppCompatActivity {
+    private GifImageView gifImageView;
+    private ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        // Start home activity
+        //startActivity(new Intent(SplashActivity.this, MainActivity.class));
+
+        // close splash activity
+        //finish();
+        setContentView(R.layout.layout);
+        gifImageView=(GifImageView) findViewById(R.id.gifImageView);
+        progressBar=(ProgressBar) findViewById(R.id.progressBar);
+        progressBar.setVisibility(progressBar.VISIBLE);
+        try{
+            InputStream inputStream=getAssets().open("giphy (1).gif");
+            byte[] bytes = IOUtils.toByteArray(inputStream);
+            gifImageView.setBytes(bytes);
+            gifImageView.startAnimation();
+        }
+        catch (IOException ex)
+        {
+
+        }
+        //wait for 3 seconds
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                SplashActivity.this.startActivity(new Intent(SplashActivity.this, LoginActivity.class));
+                //for checking googleMapActivity
+                //SplashActivity.this.startActivity(new Intent(SplashActivity.this,MapsActivity.class));
+                SplashActivity.this.finish();
+            }
+        },3000);
+    }
+}
